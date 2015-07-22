2ef099ad5883f13a4e2f7206fedf4cc0d037d2df
Twist007 <h_bratoev@abv.bg>
2015-07-22 11:10:44 +0300

    1

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Programi/wamp/www/admin/');
+define('DIR_SYSTEM', 'E:/Programi/wamp/www/system/');
+define('DIR_LANGUAGE', 'E:/Programi/wamp/www/admin/language/');
+define('DIR_TEMPLATE', 'E:/Programi/wamp/www/admin/view/template/');
+define('DIR_CONFIG', 'E:/Programi/wamp/www/system/config/');
+define('DIR_IMAGE', 'E:/Programi/wamp/www/image/');
+define('DIR_CACHE', 'E:/Programi/wamp/www/system/cache/');
+define('DIR_DOWNLOAD', 'E:/Programi/wamp/www/system/download/');
+define('DIR_UPLOAD', 'E:/Programi/wamp/www/system/upload/');
+define('DIR_LOGS', 'E:/Programi/wamp/www/system/logs/');
+define('DIR_MODIFICATION', 'E:/Programi/wamp/www/system/modification/');
+define('DIR_CATALOG', 'E:/Programi/wamp/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sait_tu');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Programi/wamp/www/catalog/');
+define('DIR_SYSTEM', 'E:/Programi/wamp/www/system/');
+define('DIR_LANGUAGE', 'E:/Programi/wamp/www/catalog/language/');
+define('DIR_TEMPLATE', 'E:/Programi/wamp/www/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/Programi/wamp/www/system/config/');
+define('DIR_IMAGE', 'E:/Programi/wamp/www/image/');
+define('DIR_CACHE', 'E:/Programi/wamp/www/system/cache/');
+define('DIR_DOWNLOAD', 'E:/Programi/wamp/www/system/download/');
+define('DIR_UPLOAD', 'E:/Programi/wamp/www/system/upload/');
+define('DIR_MODIFICATION', 'E:/Programi/wamp/www/system/modification/');
+define('DIR_LOGS', 'E:/Programi/wamp/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sait_tu');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
